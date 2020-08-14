e82219928c7e74b6e86ea1b639d52d192968abb6
lapron3000 <alejandroparparcen@yahoo.com>
2020-08-13 21:22:01 -0500

    added express ejs and bootstrap functionalities. still missing the email part.

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const app = express();
+var path = require('path');
+const port = 3000;
 
-app.
+//view engine. aka ejs
+app.set('views', path.join(__dirname, 'views'));
 
-app.listen(3000, () => {
-    console.log('server started on port 3000');
+app.set('view engine', 'ejs');
+
+//set public folder
+app.use(express.static('./public/'));
+app.use(express.static(__dirname+'/node_modules/bootstrap/dist/css/'));
+app.use(express.static(__dirname+'/node_modules/bootstrap/dist/js/'));
+
+//renders pages when asked
+app.get('/', (req, res) => {
+    res.render('pages/home');
+})
+
+
+//listens to requests by user
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
 });
